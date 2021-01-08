e3fddb952309ea6a5bb86f8d8d8918549b19a0ba
Daniel Santos <dsantos0495@gmail.com>
2021-01-08 11:02:45 -0500

    Creando custom engine parte final

--- template-engine/express-jsx.js
+++ template-engine/express-jsx.js
@@ -1,7 +1,23 @@
 const fs = require('fs');
 
+function getKeysFromOptions(options) {
+   /* Así sacamos propiedades de un objeto con destructuring, el objectKeys es la variable
+   donde quedan las demás propiedades y las que vamos a usar */
+   const { settings, _locals, ...objectKeys } = options;
+   return Object.keys(objectKeys);
+}
+
 function getRenderedContent(template, data) {
-   
+   const keys = getKeysFromOptions(data);
+   let contentString = template.toString();
+
+   for (let key of keys) {
+      contentString = contentString.replace(
+         new RegExp(`\{${key}\}`, "gi"),
+         data[key]
+      );
+   }
+   return contentString;
 }
 
 function expressJS(filePath, options, callback) {
